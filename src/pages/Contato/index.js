e5802a72bb7e1bea5f114098cfab34bb0c0d0590
André Filipe Teixeira de Lima <andrefilipedelima@hotmail.com>
2024-01-24 10:52:57 -0300

    aula 1 - routes

---
+++ src/pages/Contato/index.js
@@ -0,0 +1,16 @@
+
+import { Link } from 'react-router-dom';
+
+function Contato() {
+    return (
+      <div>
+        <h1>Página Contato</h1> 
+        <span>Contato da empresa (dd) xxxxx-xxxx</span><br /><br />
+
+        <Link to="/">Home</Link>
+      </div>
+    );
+  }
+  
+  export default Contato;
+  
---
+++ src/pages/Home/index.js
@@ -0,0 +1,17 @@
+
+import { Link } from 'react-router-dom';
+
+function Home() {
+    return (
+      <div>
+        <h1>Bem-vindo a página Home</h1> 
+        <span>Sujeito Programador</span><br /><br />
+
+        <Link to="/sobre">Sobre</Link><br />
+        <Link to="/contato">Contato</Link>
+      </div>
+    );
+  }
+  
+  export default Home;
+  
